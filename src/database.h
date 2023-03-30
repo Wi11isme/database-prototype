559e7bb1cb359480eb1b727ab0a58fb7b455e2d3
Willis Melodi <willisme@ev-d3.kzn.21-school.ru>
2023-03-30 16:49:35 +0300

    hehe

--- src/database.h
+++ src/database.h
@@ -1,8 +1,8 @@
-#define ENTITY
+// #define ENTITY
 
 //////////////////////////////////////////////////////
-ENTITY *select(FILE *db, int id);
-int delete (FILE *db, int id);
-int insert(FILE *db, ENTITY *entity);
-int update(FILE *db, int id, ENTITY *entity);
+// ENTITY *select(FILE *db, int id);
+// int delete (FILE *db, int id);
+// int insert(FILE *db, ENTITY *entity);
+// int update(FILE *db, int id, ENTITY *entity);
 //////////////////////////////////////////////////////
